a29c765572df8f037a1808873d4e6311fe8ea4fa
adityahalda <135413626+adityahalda@users.noreply.github.com>
2023-08-27 11:46:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
+
+//Define the width & height of the rover image.
+
+
+
+background_image = "mars.jpg";
+
+rover_image = "rover.png";
+
+rover_x = 10;
+rover_y = 10;
+
+//Create "add()" function
+ 
+	                                     //defining a variable with a new image
+	                                     // setting a function, onloading this variable
+	                                     // load image
+
+                                         //defining a variable with a new image
+	                                     // setting a function, onloading this variable
+	                                     // load image
+
+//Create "uploadBackground()" function.
+                              
+                                         //Draw image of background
+
+//Create "uploadrover()" function.
+
+                                        //Draw image of rover
+
+
+window.addEventListener(                            );
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+    //Write code if keypressed is up. ASCII value of "up" arrow is 38.
+		
+    
+
+
+     //Write code if keypressed is down. ASCII value of "up" arrow is 40.
+		
+
+
+
+
+    //Additional Activity
+    //Write the code for left and right arrow pressed. 
+}
+
+function up(){
+
+}
+function down(){
+	
+}
+function right(){
+	
+}
+function left(){
+	
+}
+
+
+
